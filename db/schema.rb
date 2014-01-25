352c5a2c76d6aa75ddec783e48c19d680da01b8f
Jeffrey Tong <trigun0x2@gmail.com>
2014-01-25 14:18:05 -0500

    new arena json responses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124210304) do
+ActiveRecord::Schema.define(:version => 20140125165718) do
 
   create_table "announcements", :force => true do |t|
     t.text      "body"
@@ -147,21 +147,21 @@ ActiveRecord::Schema.define(:version => 20140124210304) do
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email",                                 :null => false
-    t.string    "encrypted_password",                    :null => false
-    t.string    "reset_password_token"
-    t.timestamp "reset_password_sent_at"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",          :default => 0, :null => false
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at",                            :null => false
-    t.timestamp "updated_at",                            :null => false
-    t.integer   "tourny_id"
-    t.boolean   "guest"
-    t.string    "apikey"
+    t.string   "email",                                 :null => false
+    t.string   "encrypted_password",                    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0, :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.integer  "tourny_id"
+    t.boolean  "guest"
+    t.string   "userkey"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
